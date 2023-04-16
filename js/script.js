a66becc75674021896bc51b5aecb4ef4a294e518
Kenzyps <128271862+Kenzyps@users.noreply.github.com>
2023-04-16 11:35:14 -0300

    marktimer
    i added a marktime system, works with timer in paused and active state.
    Maybe i will add a table with markedtimes in future

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
+//tempo
 const minutesEl = document.querySelector("#minutes")
 const secondsEl = document.querySelector("#seconds")
 const milisecondsEl = document.querySelector("#miliseconds")
+// tempo marcador
+const minutesMark = document.querySelector('#minutesMark');
+const secondsMark = document.querySelector('#secondsMark')
+const milisecondsMark = document.querySelector('#millisecondsMark');
+
+//botoes
 const startBtn = document.querySelector("#startBtn")
 const pauseBtn = document.querySelector("#pauseBtn")
 const resumeBtn = document.querySelector("#resumeBtn")
 const resetBtn = document.querySelector("#resetBtn");
+const markBtn = document.querySelector("#markBtn");
 
 let interval;
 let minutes = 0;
@@ -12,10 +20,14 @@ let seconds = 0;
 let miliseconds = 0;
 let isPaused = false;
 
+//verificador dos botoes
 startBtn.addEventListener("click", startTimer);
 pauseBtn.addEventListener('click', pauseTimer);
 resumeBtn.addEventListener('click', startTimer);
 resetBtn.addEventListener('click', resetTimer)
+markBtn.addEventListener('click', markTimer);
+
+markBtn.style.display = 'none';
 
 function startTimer(){
     isPaused = false;
@@ -25,6 +37,9 @@ function startTimer(){
     startBtn.style.display = "none";
     pauseBtn.style.display = "block";
     resumeBtn.style.display = "none";
+    markBtn.style.display = 'block';
+    resetBtn.style.display = 'block';
+    
 }
 
 function workCounter() {
@@ -59,14 +74,28 @@ function resetTimer() {
     seconds = 0;
     miliseconds = 0;
 
-    minutesEl.textContent = "00";
-    secondsEl.textContent = "00";
-    milisecondsEl.textContent = "000";
+    minutesEl.textContent = '00';
+    secondsEl.textContent = '00';
+    milisecondsEl.textContent = '00';
+
+    minutesMark.textContent = '00';
+    secondsMark.textContent = '00';
+    milisecondsMark.textContent = '00';
 
     startBtn.style.display = "block";
     pauseBtn.style.display = "none";
     resumeBtn.style.display = "none";
+    markBtn.style.display = "none";
+    resetBtn.style.display = 'none';
 
+}
+
+function markTimer()
+{
+    minutesMark.textContent = formatTime(minutes);
+    secondsMark.textContent = formatTime(seconds);
+    milisecondsMark.textContent = formatMilliSeconds(miliseconds);
+    //alert('test');
 }
 
 function formatTime(time) {
@@ -83,6 +112,6 @@ function formatMilliSeconds(time){
         return time.toString.padStart(3, "0");
     }
     else{
-        return time.toString();
+        return time.toString().padStart(3, '0');
     }
 }
